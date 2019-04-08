adbfa96006affcff222366f6ffc424e268c4d32f
Leighton Chen <leighton@asia.com>
2019-04-08 14:59:34 -0700

    Crawl MS2 news site every half hour
    Inserts any non-existing news items into database and posts into
    the #announcements channel.

--- index.js
+++ index.js
@@ -1,44 +1,34 @@
 const Discord = require('discord.js');
 const Crawler = require('simplecrawler');
 const cheerio = require('cheerio');
-const { token, channel } = require('./config.js');
+const insertIfNotExists = require('./database/controllers/ms2news');
+const { token, channel } = require('./config');
 
 const client = new Discord.Client();
 const crawler = new Crawler('http://maplestory2.nexon.net/en/news');
 
 crawler.maxDepth = 1;
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('pong');
-  }
-});
-
-crawler.on("fetchcomplete", (_, responseBuffer) => {
-  const announcementChannel = client.channels.find('name', channel);
-  let $ = cheerio.load(responseBuffer.toString());
-  let items = $('.news-item');
-  for (let i = 0; i < items.length; i++) {
-    let re = /'(.*)'/;
-    let [_, image] = re.exec($(items[i]).find('.news-item-image').css('background-image'));
-    let item = {
+crawler.on('fetchcomplete', async (_, responseBuffer) => {
+  const newsChannel = client.channels.find('name', channel);
+  const $ = cheerio.load(responseBuffer.toString());
+  const items = $('.news-item');
+  const re = /'(.*)'/;
+  for (let i = items.length - 1; i >= 0; i -= 1) {
+    const [, image] = re.exec($(items[i]).find('.news-item-image').css('background-image'));
+    const item = {
       title: $(items[i]).find('h2').text(),
       description: $(items[i]).find('.short-post-text').text(),
-      url: `http://maplestory2.nexon.net${$(items[i]).find('a').attr('href')}`,
+      fullUrl: `http://maplestory2.nexon.net${$(items[i]).find('a').attr('href')}`,
       image,
     };
 
-    let msg = new Discord.RichEmbed(item);
-    msg.setImage(item.thumbnail);
-    if (i === 0) announcementChannel.send(msg);
+    // eslint-disable-next-line no-await-in-loop
+    await insertIfNotExists(item, newsChannel);
   }
 });
 
 (async () => {
   await client.login(token);
-  crawler.start();
+  setInterval(() => crawler.start(), 60000 * 30);
 })();
